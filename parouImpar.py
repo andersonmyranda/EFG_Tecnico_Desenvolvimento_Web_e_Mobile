ecf88e99fc14c19c90f118d90ca7d3da23ac2d82
andersonmyranda <92810551+andersonmyranda@users.noreply.github.com>
2023-03-24 08:04:42 -0300

    add par ou impar

---
+++ parouImpar.py
@@ -0,0 +1,6 @@
+numero = int(input('Informe um número inteiro: '))
+if numero % 2 != 0:
+    print(f'O n° {numero} informado é ÍMPAR!!')
+else:
+    print(f'O n° {numero} informado é PAR!!')
+print('-----Fim-----')
